e03491e3b16684bd9119d28c92b0b056d56fffb0
KARISHMASHINDE <52730140+KARISHMASHINDE@users.noreply.github.com>
2020-11-27 22:58:30 +0530

    Update Mycart.py
    Refactorization of code

--- Mycart.py
+++ Mycart.py
@@ -1,101 +1,185 @@
 """
 Author: Karishma Shinde
 Title: This code is for View the products,Adding the products and Billing
-Date Created: 26-11-2020
+Date Created: 27-11-2020
 """
 
-lst_category=[]
-lst_items=[]   
-category={'Mens Clothing':[{'Shirts':{'color':['Red','Black'],'size':['S','M','L'],'price':[5555,990,1000]}},],
-          'Ladies Clothing':[{'Tops':{'color':['Red','Black'],'size':['S','M','L'],'price':[9999,3333,1000]}},],
-          'Kids Clothing':[{'Sweater':{'color':['Red','Black'],'size':['S','M','L'],'price':[999,4444,1000]}},]}
+import mysql.connector
 
-'''To Display Avalilable Category'''
+con = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  database = "MyCart"
+)
 
-show_category = input("You want to see available Category? [y/n] : ")
-if show_category =='y':
-    for key, value in category.items() :
-        lst_category.append(key)
-    print("These are available Categories  : {}".format(list(enumerate(lst_category))))
+db_cursor = con.cursor() 
+
+'''Steps to Create Database and Table we required'''
+
+# db_cursor.execute("CREATE DATABASE MyCart")
+# print(db_cursor)
+
+# db_cursor.execute('SHOW DATABASES')
+# print(db_cursor)
+# for db in db_cursor:
+#     print(db)
+    
+# db_cursor.execute('SHOW TABLES')
+# print(db_cursor)
+# for db in db_cursor:
+#     print(db)
+
+
+# db_cursor.execute("CREATE TABLE bill_script (billid INT AUTO_INCREMENT PRIMARY KEY ,product_count INT(5),actual_amt INT(5),discount INT(5),final_amt INT(5))")
+# print(db_cursor)
+
+#db_cursor.execute("CREATE TABLE  product ( productid INT AUTO_INCREMENT PRIMARY KEY,categoryid INT(5), productName VARCHAR(255), productPrice INT(5),FOREIGN KEY(categoryid) REFERENCES category (categoryid))")
+
+'''Operations to perform by Admin side'''
+
+print("---------------------ADMIN PART------------------------")
+
+add_category = input("you want to add category? [y/n] : ")
+print()
+if add_category == 'y' or add_category =='Y': 
+    name = input("Enter the categoryName:")
+    print()    
+    insert_query = "INSERT INTO category (categoryName) VALUES (%s)"
+    db_cursor.execute(insert_query,(name,))
+    print("Your category {} is sucessfully added".format(name))
+con.commit()   
+
+def showCategory():
+    db_cursor.execute("SELECT * FROM category")
+    category = db_cursor.fetchall()
+    print("These are available Categories with (categoryid,categoryName)  :\n{}".format(category))
     print()
-else:
-    print("Please enter y to see Categories of product")
-    exit()
+
+add_product = input("you want to add product? [y/n] : ")
+print()
+if add_product == 'y' or add_product =='Y': 
+    showCategory()
+    categoryid = int(input("Enter the categoryid:")) 
+    productName = input("Enter the productName:")
+    productPrice = int(input("Enter the productPrice:"))
+    print() 
+    insert_query = 'INSERT INTO product (categoryid,productName,productPrice) VALUES (%s,%s,%s)'
+    db_cursor.execute(insert_query,(categoryid,productName,productPrice))
+    print("Your product {} is sucessfully added".format(productName))
+con.commit()  
+
+show_bill = input("Do you want to see User bills? [y/n] : ")
+print()
+if show_bill == 'y' or show_bill =='Y':    
+    db_cursor.execute("SELECT * FROM bill_script")
+    bills = db_cursor.fetchall()
+    print("[(bill_id, product_count, actual_amt, dis, final_amt)]")
+    print(bills)
+    print()
     
-size=[]
-cart_items = []  
-cost_items = []   
-def details():
+elif show_bill == 'n' or show_bill =='N': 
+    pass
+
+'''Operations to perform by User side'''
+
+print("---------------------User PART------------------------")
+
+show_category = input("you want to see list of category? [y/n] : ")
+print()
+if show_category == 'y' or show_category =='Y':
+    showCategory()
+elif show_category == 'n' or show_category =='N': 
+    pass
+
+show_product = input("Do you want to see list of product? [y/n] : ")
+print()
+if show_product == 'y' or show_product =='Y':    
+    choice = int(input("Enter the category id to get products:"))    
+    db_cursor.execute("SELECT productid,productName FROM product WHERE categoryid={}".format(choice))
+    product = db_cursor.fetchall()
+    print("These are available Products with (productid,productName) :\n{}".format(product))
+    print()
+elif show_product == 'n' or show_product =='N': 
+    pass
     
-    '''To Display Avalilable Products'''   
-    choice = int(input("Enter the Category id to get more product :"))
-
-    if choice < len(lst_category):
-        product_length=len(category[lst_category[choice]])
-
-        for length in range(product_length):   
-            product=category[lst_category[choice]][length]
-
-            for key, value in product.items() :
-                lst_items.append(key)
-        print("These are available Products : {}".format(list(enumerate(lst_items))))
+show_productDetails = input(" Do you want to see details of product? [y/n] : ")
+print()
+if show_productDetails == 'y' or show_productDetails =='Y':    
+    choice = int(input("Enter the product id to get details:")) 
+    print()
+    db_cursor.execute("SELECT productid,productName,productPrice FROM product WHERE productid={}".format(choice))
+    product = db_cursor.fetchall()
+    print("These are  Products details with (productid,productName,productPrice) :\n{}".format(product))
+    print()
+    
+    
+product_list = []
+cart_items=[]
+price = []
+while 1:
+    checkout = input("Do you want to add product in shopping cart? [y/n] : ")
+    print()
+    if checkout == 'y' or checkout =='Y':    
+        order = input("Enter the product you want to add in cart:")
+        cart_items.append(order) 
+        print("you have {} products in yor cart {}".format(len(cart_items),cart_items))
         print()
-        
-        '''To Display Avalilable Product DEtails'''
-        
-        details = int(input("Enter the Product id to get more product details :"))
-        if details<= len(lst_items):    
-            product_details=category[lst_category[choice]][details]
-            print(product_details)
-        else:
-            print("Please enter correct Product id") 
-            exit()
-details ()
 
-def checkout():
-    while 1:
-        print()
-        cart = input("You want to add product in cart[y/n] :")
         
-        if cart == 'y':
-            order = input("Please select the product want to add in cart : ")
-            cart_items.append(order)
-            product_size =input("Please select the size of product(Enter 0 for S,1 for M,2 for L) :")
-            size.append(product_size)
-            x= zip(cart_items,size)
-            cart_products=(list(x))
+    elif checkout == 'n' or checkout =='N':
+        bill = input("You want to checkout?[y/n] : ")
+        if bill == 'y' or bill =='Y':
+            remove_product = input(" Do you want to remove product from cart? [y/n] : ")
             print()
-            print("you have {} products in yor cart {}".format(len(cart_products),cart_products))
+            if remove_product == 'y' or remove_product =='Y': 
+                print("Enter the product you want to remove from cart:")
+                removeit = input()
+                cart_items.remove(removeit)
+                print()
+            elif remove_product == 'n' or remove_product =='N': 
+                pass
             
-            checkout = input("You want to checkout?[y/n] : ")
-            if checkout == 'y':
-                for x in range(len(cart_products)):
-                    bill_list = (cart_products[x])
-                    if bill_list[0] == 'Shirts':
-                        cost = (category.get('Mens Clothing',{})[0].get(bill_list[0]).get('price')[int(bill_list[1])])
-                        cost_items.append(cost)
-                    elif bill_list[0] == 'Tops':
-                        cost = (category.get('Ladies Clothing',{})[0].get(bill_list[0]).get('price')[int(bill_list[1])])
-                        cost_items.append(cost)
-                    elif bill_list[0] == 'Sweater':
-                        cost = (category.get('Kids Clothing',{})[0].get(bill_list[0]).get('price')[int(bill_list[1])])
-                        cost_items.append(cost)
-                print()       
-                amt = sum(cost_items)
-                # checking conditions and calculating discount
-                if(amt>0):
-                    if amt >= 10000:
-                        disc = 500
-                    else:
-                        disc=0
-                        
-                    print("Actual Amount : ",amt)
-                    print("Discount      : ",disc)
-                    print("Final Amount  : ",amt-disc)
-                    exit()
+            for items in cart_items:
+                sql = "SELECT productName,productPrice FROM product WHERE productName = %s"
+                adr = (items, )
+                db_cursor.execute(sql,adr)
+                product = db_cursor.fetchall()
+                product_list.append(product)                
+                
+            print('Products in cart with (productName,productPrice) :')
+            for x in product_list:
+                print(x)
+                
+            for y in product_list:
+                for i in y:
+                    price.append(i[1])
+            amt = sum(price)
+            if(amt>0):
+                if amt >= 10000:
+                    disc = 500
                 else:
-                    print("Invalid Amount")
-                    exit()
-                    
-checkout()
+                    disc=0
+                print()  
+                print() 
+                print("Actual Amount : ",amt)
+                print("Discount      : ",disc)
+                print("Final Amount  : ",amt-disc)
+                
+                product_count = len(cart_items)
+                insert_query = 'INSERT INTO bill_script (product_count,actual_amt,discount,final_amt) VALUES (%s,%s,%s,%s)'
+                db_cursor.execute(insert_query,(product_count,amt,disc,amt-disc))
+                con.commit() 
+                print("Thank You,Keep Shopping!!!!")
+                exit()
+            else:
+                print("Invalid Amount")
+                exit()
+        elif bill == 'n' or bill =='N':
+            print("Thank You for visiting our website!!!!")
+            exit()
+    else:
+        print("Invalid answer")
+con.commit()   
+db_cursor.close()
 
